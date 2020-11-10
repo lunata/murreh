da1c4691863f07342bd156fe480dcb9a0594559c
lunata <nataly@krc.karelia.ru>
2020-11-10 11:38:25 +0300

    small

--- resources/views/service/index.blade.php
+++ resources/views/service/index.blade.php
@@ -14,7 +14,7 @@
     
     <h3>Исправление данных</h3>
     <p><a href="service/add_sequence_number_to_questions">Перенумеровать вопросы</a> (вставить пустые номера)</p>
-    <p><a href="service/add_sequence_number_qsections">Перенумеровать разделы вопросов</a> (вставить пустые номера)</p>
+    <p><a href="service/add_sequence_number_to_qsections">Перенумеровать разделы вопросов</a> (вставить пустые номера)</p>
     <p><a href="service/split_qsections">Разбить разделы вопросов</a></p>
     
 @endsection
