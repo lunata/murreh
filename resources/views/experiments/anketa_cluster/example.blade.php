36ddb150e4aff8cb97c539d9172449aad1e1c512
lunata <nataly@krc.karelia.ru>
2022-04-16 15:33:01 +0300

    small

--- resources/views/experiments/anketa_cluster/example.blade.php
+++ resources/views/experiments/anketa_cluster/example.blade.php
@@ -42,6 +42,7 @@
             <div class="marker-icon marker-legend marker-{{$cl_colors[$cl_num]}}">
             <!--img src="/images/markers/marker-icon-{{$cl_colors[$cl_num]}}.png" style="padding-right: 5px; margin-top:-10px"-->
             <span><b>{{$cl_num}}</b> ({{sizeof($cluster)}}):</span>
+            </div>
         </div>
        {{\App\Models\Geo\Place::namesWithDialectsByIdsToString($cluster)}}
        <!--br><span style="font-style: italic; color:grey">{{join(', ', \App\Models\Ques\AnketaQuestion::getAnswersForPlacesQsections($cluster, $qsection_ids, $question_ids))}}</span-->
