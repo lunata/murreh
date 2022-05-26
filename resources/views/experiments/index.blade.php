e3f5f9bab0b3db21364e6142c75537eeb8252b71
lunata <nataly@krc.karelia.ru>
2022-05-27 01:09:24 +0300

    small

--- resources/views/experiments/index.blade.php
+++ resources/views/experiments/index.blade.php
@@ -11,12 +11,13 @@
     <li><a href='/experiments/anketa_cluster/example/1_2'>Раздел "Дифтонги", метод полной связи</a></li>
     <li><a href='/experiments/anketa_cluster/example/5_2'>Раздел "Дифтонги", метод одиночной связи</a></li>
     <li><a href='/experiments/anketa_cluster/example/1_3-119'>Разделы "Звонкий / глухой согласный", метод полной связи</a></li>
+    <li><a href='/experiments/anketa_cluster/example/1_1_7'>Разделы "Гласный конца слова", метод полной связи</a></li>
     </ul>
     <p><a href="/experiments/sosd_cluster">{{trans('navigation.sosd_cluster')}}</a></p>
     <p>Примеры:</p>
     <ul>
-    <li><a href='/experiments/sosd_cluster/example/1_all'>{{__('navigation.sosd_cluster_all')}}</a></li>
-    <li><a href='/experiments/sosd_cluster/example/1_207swadesh'>{{__('navigation.sosd_cluster_207swadesh')}}</a></li>
-    <li><a href='/experiments/sosd_cluster/example/1_100swadesh'>{{__('navigation.sosd_cluster_100swadesh')}}</a></li>
+    <li><a href='/experiments/sosd_cluster/example/1_1_all'>{{__('navigation.sosd_cluster_all')}}</a></li>
+    <li><a href='/experiments/sosd_cluster/example/1_1_207swadesh'>{{__('navigation.sosd_cluster_207swadesh')}}</a></li>
+    <li><a href='/experiments/sosd_cluster/example/1_1_100swadesh'>{{__('navigation.sosd_cluster_100swadesh')}}</a></li>
     </ul>
 @endsection
